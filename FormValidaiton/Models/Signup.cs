bc81351f39640dd9df25c5a184513e51a9d70d15
Shoaib <shoaibrochi9@gmail.com>
2023-10-10 12:53:37 +0600

    Update: Signup validation (added: UId validation)

--- FormValidaiton/Models/Signup.cs
+++ FormValidaiton/Models/Signup.cs
@@ -8,13 +8,15 @@ namespace FormValidaiton.Models
 {
     public class Signup
     {
-        [Required(ErrorMessage = "Please provide valid name")]
+        [Required(ErrorMessage = "Name field cannot be empty")]
      //   [StringLength(50,MinimumLength =5 ,ErrorMessage="Name must be between 5 and 50 characters.")]
-        [RegularExpression(@"^[a-z-A-Z.\s]{5,50}$", ErrorMessage = "Please input valid name with length 5~50")]
+        [RegularExpression(@"^[a-z-A-Z.\s]{5,50}$", ErrorMessage = "Please provide valid name with length 5~50")]
 
         public string Name { get; set; }
-        
 
+        [Required(ErrorMessage = "User Id cannot be empty")]
+        [RegularExpression(@"^(?!.*[-_]{2,})[a-z-_A-Z0-9]{4,12}$", ErrorMessage = "Please provide an unique user id with length 4~12")]
+        public string UId { get; set; }
 
     }
 }
