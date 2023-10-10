fbc60186d4eac341d1d632ced2fb345cc9fcb1d0
Shoaib <shoaibrochi9@gmail.com>
2023-10-10 11:46:38 +0600

    Update: Signup validation

--- FormValidaiton/Models/Signup.cs
+++ FormValidaiton/Models/Signup.cs
@@ -10,7 +10,7 @@ namespace FormValidaiton.Models
     {
         [Required(ErrorMessage = "Please provide valid name")]
      //   [StringLength(50,MinimumLength =5 ,ErrorMessage="Name must be between 5 and 50 characters.")]
-        [RegularExpression(@"^[a-z.A-Z\-\s]{5,50}$", ErrorMessage = "Please input valid name with length 5~50")]
+        [RegularExpression(@"^[a-z-A-Z.\s]{5,50}$", ErrorMessage = "Please input valid name with length 5~50")]
 
         public string Name { get; set; }
         
